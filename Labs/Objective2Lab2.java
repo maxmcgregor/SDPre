d68509741bc50cf348c1197bd23097d892239d6f
Max McGregor <maxmcgregor1@gmail.com>
2021-08-27 16:49:20 -0500

    finish Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Max McGregor";
+    int age = 30;
+    String state = "New York";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
