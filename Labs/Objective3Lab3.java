7777fba8488df760eefeb5b81ed81bd49f12b391
Max McGregor <maxmcgregor1@gmail.com>
2021-08-27 17:45:39 -0500

    complete Objective3Lab3 with string corrections

--- Labs/Objective3Lab3.java
+++ Labs/Objective3Lab3.java
@@ -4,6 +4,10 @@ public class Objective3Lab3 {
     int second = 1 + (int)(Math.random() * 6);
     int third = 1 + (int)(Math.random() * 6);
     int sum = first + second + third;
-    System.out.println(sum);
+    String stringFirst = String.valueOf(first);
+    String stringSecond = String.valueOf(second);
+    String stringThird = String.valueOf(third);
+    String stringSum = String.valueOf(sum);
+    System.out.println(stringFirst + " + " + stringSecond + " + " + stringThird + " = " + stringSum);
   }
 }
