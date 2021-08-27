7159587593ccd9ef44f44e803ded3e18d1e642e5
Max McGregor <maxmcgregor1@gmail.com>
2021-08-27 16:54:49 -0500

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main (String[] args) {
+    double num1, num2, num3, average;
+    num1 = 6;
+    num2 = 23;
+    num3 = 91;
+    average = (num1 + num2 + num3) / 3;
+    System.out.println(average);
+  }
+}
