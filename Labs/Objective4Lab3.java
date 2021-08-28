4514dafb62d27ebd0bc8807ee778699bd8072555
Max McGregor <maxmcgregor1@gmail.com>
2021-08-28 13:03:41 -0500

    completed Objective4Lab3

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Objective4Lab3 {
+  public static void main(String[] args) {
+    int birthYear, age, currentYear;
+
+    birthYear = 0;
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("How old are you?");
+    birthYear = 2021 - input.nextInt();
+
+    System.out.println("You were born in " + birthYear);
+    input.close();
+  }
+}
